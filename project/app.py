1438f90b33a89e47148ebe9ecf7007f6cfd853fc
Pakawat Phasook <skb@Pakawats-MacBook-Air.local>
2024-10-04 15:21:35 -0400

    Add a new import in app.py

--- project/app.py
+++ project/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import sqlite3
-from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort, jsonify
+
 # configuration
 DATABASE = "flaskr.db"
 USERNAME = "admin"
