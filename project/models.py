d0c7a7242ada2871ee55aa3e6bb2fe3a77453040
Pakawat Phasook <skb@Pakawats-MacBook-Air.local>
2024-10-04 17:51:29 -0400

    make it better

--- project/models.py
+++ project/models.py
@@ -1,14 +1,16 @@
-from project.app import db
-
+# project/models.py
+from project import db
 
 class Post(db.Model):
+    __tablename__ = 'post'
+    
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    text = db.Column(db.String, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    text = db.Column(db.Text, nullable=False)
 
     def __init__(self, title, text):
         self.title = title
         self.text = text
 
     def __repr__(self):
-        return f"<title {self.title}>"
+        return f"<Post {self.title}>"
